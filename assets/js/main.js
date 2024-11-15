a347636f56d3c066b9f18aea777406286df61429
JosueLevi <122812809+JosueLevi@users.noreply.github.com>
2024-11-15 17:12:40 -0500

    update

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 1,
+    spaceBetween: 70,
+    grabCursor: true,
+    
+    breakpoints: {
+        991: {
+            slidesPerView: 3,
+        }
+    }
+
+})
