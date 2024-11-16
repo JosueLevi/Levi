0686328105a2af80d13ad55b93026126ec6c22b5
JosueLevi <friaslevi97@gmail.com>
2024-11-16 00:35:35 -0500

    Middle

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@ var swiper = new Swiper(".mySwiper", {
 
     slidesPerView: 1,
     spaceBetween: 70,
-    grabCursor: true,
-    
+    // grabCursor: true,
+    // loop:true,
     breakpoints: {
         991: {
             slidesPerView: 3,
